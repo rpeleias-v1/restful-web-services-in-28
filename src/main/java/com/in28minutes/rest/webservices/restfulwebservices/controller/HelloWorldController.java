26949a82898375c9d1db19610dd16102fe4d8f9a
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-03-10 21:53:57 -0300

    Step 07 developed

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.controller;
 
+import com.in28minutes.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
