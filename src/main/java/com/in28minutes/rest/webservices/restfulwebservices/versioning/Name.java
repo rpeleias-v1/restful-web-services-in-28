466e91a02dd13fbf34aeb89345ad807188f6ec8a
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2019-03-18 23:05:56 -0300

    Added first part or versioning control

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/Name.java
@@ -0,0 +1,16 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Name {
+
+    private String firstName;
+    private String lastName;
+
+
+}
---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,15 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonV1 {
+
+    private String name;
+}
